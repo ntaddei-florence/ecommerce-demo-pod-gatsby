4ac4cd3eaeb177db1b8558965d46a34e524844a4
fabiofognani <ffognani@florence-consulting.it>
2023-08-30 15:59:11 +0200

    feat: added env sample file; adding gatsby plugins;

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,16 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-commercelayer`,
+  ],
+
 }
